2400dadc1f547b62d1e178eaa2fa9a1eb7fd250a
TedCreator <takahori2@gmail.com>
2023-11-11 03:03:39 -0500

    Added Registry change to reflect accel change.

--- DeAcceleration.cpp
+++ DeAcceleration.cpp
@@ -1,6 +1,7 @@
 // Copyright 2023 Ted Akahori
 #include <iostream>
 #include <Windows.h>
+#include "DeAcceleration.h"
 using std::cout;
 using std::endl;
 using std::string;
@@ -25,7 +26,11 @@ LSTATUS RegNotifyChangeKeyValue(
 );
 */
 
+// Used in SystemParametersInfoW
 int pvParam[3];
+
+// Used to edit registry values.
+HKEY hKey;
 
 bool AccelStatus(){
     // Mouse Accel on: [MT1 = 6, MT2 = 10, MouseSpeed = 1].
@@ -45,15 +50,107 @@ string RegistryString(const int pvParam[3]){
     return reg;
 }
 
-void disableMouseAccel(){
+bool ZeroAccelerationInRegistry(){
+    WCHAR newVal[] = L"0";
+    DWORD valLen = (lstrlenW(newVal) + 1) * sizeof(WCHAR);
+    LONG result = RegSetValueEx(
+        hKey,
+        "MouseSpeed",
+        0,
+        REG_SZ,
+        (LPBYTE)newVal,
+        valLen
+    );
+    if(result != ERROR_SUCCESS){
+        std::cerr << "Error setting registry key, exiting.";
+        return false;
+    }
+    RegSetValueEx(
+        hKey,
+        "MouseThreshold1",
+        0,
+        REG_SZ,
+        (LPBYTE)newVal,
+        valLen
+    );
+    RegSetValueEx(
+        hKey,
+        "MouseThreshold2",
+        0,
+        REG_SZ,
+        (LPBYTE)newVal,
+        valLen
+    );
+    return true;
+}
+
+void DisableMouseAccel(){
     pvParam[0], pvParam[1], pvParam[2] = 0;
     // Seems to work to disable mouse acceleration, but doesn't change registry properly.
     SystemParametersInfoW(SPI_SETMOUSE, 0, pvParam, SPIF_SENDCHANGE);
+    
+    // Changes registry to reflect above changes.
+    ZeroAccelerationInRegistry();
 
 }
 
 int main(){
+    RegOpenKeyEx(
+        HKEY_CURRENT_USER,
+        "Control Panel\\Mouse",
+        0,
+        KEY_SET_VALUE,
+        &hKey
+    );
+
     if(AccelStatus()){
-        disableMouseAccel();
+        DisableMouseAccel();
     }
+
+    RegCloseKey(hKey);
+
+    return 0;
 }
+
+
+// HKEY hKey;
+//     // Open the Mouse Registry
+//     LONG result = RegOpenKeyEx(
+//         HKEY_CURRENT_USER,
+//         "Control Panel\\Mouse",
+//         0,
+//         KEY_NOTIFY,
+//         &hKey
+//     );
+//     if (result != ERROR_SUCCESS) {
+//         std::cerr << "Error opening registry key: " << result << endl;
+//         return 1;
+//     }
+//     // Create listening event
+//     HANDLE hEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+//     if (hEvent == nullptr) {
+//         std::cerr << "Error creating event: " << GetLastError() << endl;
+//         RegCloseKey(hKey);
+//         return 1;
+//     }
+
+//     // A forever loop since this runs until it's closed.
+//     while(true){
+//         result = RegNotifyChangeKeyValue(
+//             hKey,
+//             TRUE,
+//             REG_NOTIFY_CHANGE_LAST_SET,
+//             hEvent,
+//             TRUE
+//         );
+//         if (result != ERROR_SUCCESS) {
+//             std::cerr << "Error creating registry change event" << endl;
+//             break;
+//         }
+
+//         WaitForSingleObject(hEvent, INFINITE);
+        
+//         if(AccelStatus()){
+//             disableMouseAccel();
+//         }
+//     }
--- DeAcceleration.h
+++ DeAcceleration.h
@@ -2,7 +2,12 @@
 
 // Returns boolean corresponding to if Mouse Acceleration is on.
 bool AccelStatus();
+
 // Returns a string of values from the SPI_GETMOUSE input parameter.
-string RegistryString(const int pvParam[3]);
+std::string RegistryString(const int pvParam[3]);
+
 // Disables Mouse Acceleration using SPI_SETMOUSE & SPIF_SENDCHANGE.
-void disableMouseAccel();
+void DisableMouseAccel();
+
+// Changes Registry values to reflect disabling mouse acceleration.
+bool ZeroAccelerationInRegistry();
